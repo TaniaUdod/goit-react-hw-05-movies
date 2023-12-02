9d575bd9d79ae1bfafc2d4bef40f322d68a18083
TaniaUdod <taniaudod28@gmail.com>
2023-12-03 01:05:13 +0200

    Update Home page

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+const MovieList = ({ movies }) => {
+  const location = useLocation();
+  return (
+    <ul>
+      {movies.map(({ id, original_title }) => (
+        <li key={id}>
+          <Link to={`/movies/${id}`} state={{ from: location }}>
+            {original_title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { getTrending } from 'api/TMDBApi';
+import MovieList from 'components/MovieList';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const getTrendingMovies = () => {
+      setIsLoading(true);
+
+      getTrending()
+        .then(trendingMovies => {
+          setMovies(trendingMovies);
+        })
+        .catch(error => {
+          setError(error.message);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    };
+    getTrendingMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      {isLoading && <p>loading...</p>}
+      <MovieList movies={movies} />
+      {error && (
+        <p style={{ textAlign: 'center', margin: 'auto' }}>Sorry. {error} 😭</p>
+      )}
+    </div>
+  );
 };
 
 export default Home;
